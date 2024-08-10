82e9fd6383ad76bb128b441a62a342d5c6b6379a
muratapk <muratapk@hotmail.com>
2024-08-10 17:59:46 +0300

    Jwt Token Oluşturulması

--- GazeteApi/Controllers/NewsController.cs
+++ GazeteApi/Controllers/NewsController.cs
@@ -26,6 +26,54 @@ namespace GazeteApi.Controllers
         {
             return await _context.News.ToListAsync();
         }
+        //bir id numarasına göre haber çağırmak için kullanılan method
+
+        [HttpGet("id")]
+        public async Task<ActionResult<News>>GetNews(int id)
+        {
+            return await _context.News.FindAsync(id);
+        }
+        [HttpDelete("id")]
+        public async Task<ActionResult<News>>DeleteNews(int id)
+        {
+            var result = await _context.News.FindAsync(id);
+            if(result!=null)
+            {
+                _context.Remove(result);
+                await _context.SaveChangesAsync();
+
+            }
+            return result;
+            //boş olan veri geri gönder
+        }
+        [HttpPost]
+        public async Task<ActionResult<News>>AddNews(News news)
+        {
+            _context.News.Add(news);
+            await _context.SaveChangesAsync();
+            return Ok();
+        }
+        [HttpPut]
+        public async Task<ActionResult<News>>UpdateNews(int id,News news)
+        {
+            var result = await _context.News.FindAsync(id);
+            //veritabanın id 5 olanları getir result içine attım 
+            //eski veriler burada duruyor
+            if(result!=null)
+            {
+                //result kullanılan eski veriler
+                result.Header1 = news.Header1;//yeni veriler
+                result.Header2 = news.Header2;
+                result.CreateDate = news.CreateDate;
+                result.UpdateDate = news.UpdateDate;
+                result.CategoryId = news.CategoryId;
+                result.Content = news.Content;
+                await _context.SaveChangesAsync();
+            }
+            return result;
+        }
+
+
 
     }
 }
